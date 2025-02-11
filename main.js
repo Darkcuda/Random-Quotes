e60a78a90902e50a0d71b241c3696d2a56f3b775
Matteo54 <matteo.conci54@hotmail.it>
2025-02-11 13:32:39 +0100

    Add function to assemble the quotes and call it

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const partOne = ['Today is the', 'Yesterday was the', 'Tomorrow will be the'];
 const number = Math.floor(Math.random() * 28);
 const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+const assembleQuotes = (first, second, third) => {
+    let ranOne = Math.floor(Math.random() * 3);
+    let ranMonth = Math.floor(Math.random() * 12);
+    let quotes = [first[ranOne], second, third[ranMonth] ];
+    console.log(quotes.join(' '));
+}
+
+assembleQuotes(partOne, number, month);
