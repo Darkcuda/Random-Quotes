280c94a76eb27131f6c7dae997d6a2e9fef849ea
Matteo54 <matteo.conci54@hotmail.it>
2025-02-11 13:16:12 +0100

    Add the variables of the quotes

---
+++ main.js
@@ -0,0 +1,3 @@
+const partOne = ['Today is the', 'Yesterday was the', 'Tomorrow will be the'];
+const number = Math.floor(Math.random() * 28);
+const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
